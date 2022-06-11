b658ff0ab1179dccf04bf7231ddef7b8c00039dd
51pwn <51pwn@51pwn.com>
2022-06-12 03:26:26 +0800

    update

--- .run.sh
+++ .run.sh
@@ -1,4 +1,5 @@
 cd $HOME/MyWork/TOP
 py3 $HOME/MyWork/mybugbounty/getTop.py >end.md
-cat he.md end.md ed.md >README.md; git commit -m "update" .;git push
+cat he.md end.md ed.md >README.md; git commit -m "update ${date}" .;git push
 
+`
