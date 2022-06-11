e891ee352cd5547bbee880dc1c5207da0be0b86e
51pwn <51pwn@51pwn.com>
2022-06-12 03:29:55 +0800

    update 2022年 6月12日 星期日 03时29分55秒 CST

--- .run.sh
+++ .run.sh
@@ -1,5 +1,6 @@
 cd $HOME/MyWork/TOP
 py3 $HOME/MyWork/mybugbounty/getTop.py >end.md
-cat he.md end.md ed.md >README.md; git commit -m "update ${date}" .;git push
+cat he.md end.md ed.md >README.md
+git commit -m "update $(date)" .;git push
 
 `
