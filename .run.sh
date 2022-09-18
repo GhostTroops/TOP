d8b576d59e90675aab3a938591911224ca6c08dc
hktalent <18223385+hktalent@users.noreply.github.com>
2022-09-18 19:18:13 +0800

    x

--- .run.sh
+++ .run.sh
@@ -2,6 +2,7 @@ pwd
 python3 .github/getTop.py >end.md
 cat he.md end.md ed.md >README.md
 python3 .github/getCodeql.py
+cat Top_Codqql1.md
 cat hecdql.md Top_Codqql1.md ed.md >Top_Codeql.md
 # git commit -m "update $(date)" .;git push
 
